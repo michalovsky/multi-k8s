d63711c62ccb598b44133b78ab9a0f8f797bd783
michalovsky <michalovskyyy@gmail.com>
2020-07-20 10:02:46 +0200

    add deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,16 @@
-docker build -t michalovsky/multi-client -f ./client/Dockerfile ./client
-docker build -t michalovsky/multi-server -f ./server/Dockerfile ./server
-docker build -t michalovsky/multi-worker -f ./worker/Dockerfile ./worker
-docker push michalovsky/multi-client
-docker push michalovsky/multi-server
-docker push michalovsky/multi-worker
+docker build -t michalovsky/multi-client:latest -t michalovsky/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t michalovsky/multi-server:latest -t michalovsky/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t michalovsky/multi-worker:latest -t michalovsky/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push michalovsky/multi-client:latest
+docker push michalovsky/multi-server:latest
+docker push michalovsky/multi-worker:latest
+
+docker push michalovsky/michalovsky/multi-client:$SHA
+docker push michalovsky/michalovsky/multi-server:$SHA
+docker push michalovsky/michalovsky/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/client-deployment client=michalovsky/multi-client:$SHA
+kubectl set image deployments/server-deployment server=michalovsky/multi-server:$SHA
+kubectl set image deployments/worker-deployment worker=michalovsky/multi-worker:$SHA
